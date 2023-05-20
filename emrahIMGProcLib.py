c6f15118a017a764df626ebe7226a185cdd31828
emrahe82 <131906869+emrahe82@users.noreply.github.com>
2023-05-21 01:44:14 +0300

    Update emrahIMGProcLib.py

--- emrahIMGProcLib.py
+++ emrahIMGProcLib.py
@@ -74,13 +74,13 @@ def segmentOutImageMinimalBorder(img,yolo_results):
   return None
 
 
-def prepare_for_inference(img,target_size,segmentation_model):
+def prepare_for_inference(img,target_size,border_size=20,segmentation_model):
+  img=add_black_border(img, border_size) #first add a black border around, maybe needed for vhscollector images. for increasing segmentation accuracy
   segmentation_results=segmentation_model.predict(img)
-  a=segmentOutImageMinimalBorder(img,segmentation_results[0])
+  a=segmentOutImageMinimalBorder(img,segmentation_results[0]) #crop out the segmented image
   a=convert_to_square_image(a)
   a=cv2.resize(a, target_size, interpolation=cv2.INTER_LINEAR)
   return a
-
 
 
 #siamese model with trained weights, and extractor
