05b8a3e26f40b4465a50714f8779feae8d70c60a
emrahe82 <131906869+emrahe82@users.noreply.github.com>
2023-05-20 22:08:30 +0300

    Update emrahIMGProcLib.py

--- emrahIMGProcLib.py
+++ emrahIMGProcLib.py
@@ -1,3 +1,9 @@
+from keras.applications import Xception
+from keras import layers as lyr
+from keras.models import Model
+from keras import backend as K
+    
+
 #input is a random size image, and makes it square by adding 
 #black contours around the image
 def convert_to_square_image(img):
@@ -49,3 +55,58 @@ def prepare_for_inference(img,target_size,segmentation_model):
   a=convert_to_square_image(a)
   a=cv2.resize(a, target_size, interpolation=cv2.INTER_LINEAR)
   return a
+
+
+
+#siamese model with trained weights, and extractor
+def create_and_load_siamese_model_with_extractor(input_shape, weights='imagenet', model_weights_path=None):
+    
+    # Load the pretrained model
+    base_model = Xception(weights=weights, include_top=False, input_shape=input_shape)
+
+    # Add a GlobalAveragePooling2D layer after the base model
+    gap = lyr.GlobalAveragePooling2D()
+
+    # Define the Siamese network
+    input_left = lyr.Input(input_shape)
+    input_right = lyr.Input(input_shape)
+
+    # Pass the inputs through the base model and GlobalAveragePooling2D layer
+    encoded_left = gap(base_model(input_left))
+    encoded_right = gap(base_model(input_right))
+
+    # Compute the L1 distance between the encoded outputs
+    l1_distance = lyr.Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))([encoded_left, encoded_right])
+
+    # Add some dense layers for classification
+    x = lyr.Dense(256, activation='relu')(l1_distance)
+    x = lyr.Dropout(0.5)(x)
+    x = lyr.BatchNormalization()(x)
+
+    x = lyr.Dense(128, activation='relu')(x)
+    x = lyr.Dropout(0.5)(x)
+    x = lyr.BatchNormalization()(x)
+
+    x = lyr.Dense(128, activation='relu')(x)
+    x = lyr.Dropout(0.5)(x)
+    x = lyr.BatchNormalization()(x)
+
+    predictions = lyr.Dense(1, activation='sigmoid')(x)
+
+    # Define the Siamese network
+    siamese_model = Model(inputs=[input_left, input_right], outputs=predictions)
+
+    # Define a new input layer for a single image
+    input_single = lyr.Input(input_shape)
+
+    # Pass the input through the base model and GlobalAveragePooling2D layer
+    encoded_single = gap(base_model(input_single))
+
+    # Define the feature extraction model for a single image
+    feature_extractor_single = Model(inputs=input_single, outputs=encoded_single)
+
+    # Loading the Weights
+    if model_weights_path:
+        siamese_model.load_weights(model_weights_path)
+
+    return siamese_model, feature_extractor_single
