89d5f31efa82a219db85a6e56125264f4e30bb74
emrahe82 <131906869+emrahe82@users.noreply.github.com>
2023-05-22 03:02:53 +0300

    Update emrahIMGProcLib.py

--- emrahIMGProcLib.py
+++ emrahIMGProcLib.py
@@ -500,7 +500,7 @@ def extractFeaturesfromDataBase(current_input_row, movieDictionary, combined_dat
       if(numImageLinks==1):
         print("there is a combined image on the database, splitting with yolo segmentation!")
         print(imageLinks)
-        combined = emrahIMGProcLib.read_image_from_url(imageLinks[0])
+        combined = read_image_from_url(imageLinks[0])
 
         if(combined is None):
           print("problem with the images! : " + str(imageLinks))
@@ -512,7 +512,7 @@ def extractFeaturesfromDataBase(current_input_row, movieDictionary, combined_dat
         height=combined.shape[0]
         width=combined.shape[1]
         results=flat_image_split_model.predict(combined, save=True, boxes=True, show=False, line_thickness=4)
-        x_coords = emrahIMGProcLib.get_x_coords_from_yolo_segmentation_results(results, (width-1))
+        x_coords = get_x_coords_from_yolo_segmentation_results(results, (width-1))
 
         for j,x in enumerate(x_coords):
           if(j<(len(x_coords)-1)):
@@ -521,12 +521,12 @@ def extractFeaturesfromDataBase(current_input_row, movieDictionary, combined_dat
 
       else:
         for j,link in enumerate(imageLinks):
-          img = emrahIMGProcLib.read_image_from_url(link)
+          img = read_image_from_url(link)
           imgArray.append(img)
 
 
       for i,img in enumerate(imgArray):
-        imgReadForInference=emrahIMGProcLib.prepare_for_inference(img,target_size,20,segmentation_model)
+        imgReadForInference=prepare_for_inference(img,target_size,20,segmentation_model)
 
         if(imgReadForInference is None):
           phash_, dhash_, sift_keypoints_,  sift_descriptors_, siamese_features_ = None, None, None, None, None
@@ -534,11 +534,11 @@ def extractFeaturesfromDataBase(current_input_row, movieDictionary, combined_dat
         else:
           cv2_imshow(imgReadForInference)
 
-          corners_ = emrahIMGProcLib.detect_rectangle_via_contour(imgReadForInference)
-          corr_arr_ = emrahIMGProcLib.correct_perspective_from_contour(imgReadForInference, corners_)
+          corners_ = detect_rectangle_via_contour(imgReadForInference)
+          corr_arr_ = correct_perspective_from_contour(imgReadForInference, corners_)
 
-          phash_, dhash_ = emrahIMGProcLib.hashFinder(corr_arr_)
-          sift_keypoints_,  sift_descriptors_= emrahIMGProcLib.siftFinder(corr_arr_)
+          phash_, dhash_ = hashFinder(corr_arr_)
+          sift_keypoints_,  sift_descriptors_= siftFinder(corr_arr_)
 
           img_array = imgReadForInference / 255.0
           img_array = np.expand_dims(img_array, axis=0)
